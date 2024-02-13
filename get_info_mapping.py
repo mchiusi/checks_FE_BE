5fe2d1ac580bd893bdb6e91bc8c63e32aeea02c3
mchiusi <marco.chiusi@cern.ch>
2024-02-13 09:22:33 +0100

    producing module scatter plots

--- get_info_mapping.py
+++ get_info_mapping.py
@@ -1,26 +1,33 @@
 import pandas as pd
 import xml.etree.ElementTree as ET
 import numpy as np 
+import matplotlib.pyplot as plt
 
 import Tools as tools
 
-# tree = ET.parse('xml/S1toChannels.SeparateTD.120.MixedTypes.NoSplit.xml')
+def produce_plot(df):
+    unique_s1_values = df['S1'].unique()
+
+    for s1_value in unique_s1_values:
+        subset_data = df[df['S1'] == s1_value]
+        grouped_data = subset_data.groupby('Module').agg(
+                       {'Column': 'nunique', 'TCcount': 'max'}).reset_index()
+   
+        print(f'Analying {s1_value} containing {len(grouped_data)} modules/MB..')
+        plt.figure(figsize=(10, 6))
+        plt.scatter(grouped_data['Column'], grouped_data['TCcount']/grouped_data['Column'], alpha=0.4)
+        
+        plt.xlabel('Number of columns in each module')
+        plt.ylabel('Max TCs / Number of columns')
+        plt.title(f'Modules processed by S1 {s1_value}')
+        plt.savefig('plots/TCmax_vs_ncols'+s1_value+'.pdf')
+    
+# main
 tree = ET.parse('xml/ChannelAllocation_SeparateTD-120-MixedTypes-NoSplit.xml')
 geometry_file = 'xml/Geometry.xml'
 
 df = tools.extract_data(tree, geometry_file)
 #print(df.columns)
-
-# # number of columns per module
-# col_per_module = df[['Module', 'S1', 'plane', 'u', 'v', 'Column', 'MB']].sort_values(by=['S1', 'Module']).drop_duplicates(subset=['Module','Column'])
-# col_per_module = col_per_module.groupby(['S1','Module']).agg({
-#                      'Column': 'nunique',
-#                      'u': 'first', 
-#                      'v': 'first',
-#                      'plane': 'first',
-#                      'MB': 'first' 
-#                  }).reset_index()
-# col_per_module.to_excel("xlsx/cols_per_module.xlsx", index=False)
 
 # number of TCs per columns
 for det in df.keys():
@@ -44,4 +51,5 @@ for det in df.keys():
     if det == 'sci': result_df = pd.concat([result_df, tc_per_column])
     if det == 'si':  result_df = tc_per_column
 
-result_df.to_excel("xlsx/tc_per_column_mod_S1.xlsx")
+produce_plot(result_df.reset_index(name='TCcount'))
+# result_df.to_excel("xlsx/tc_per_column_mod_S1.xlsx")
